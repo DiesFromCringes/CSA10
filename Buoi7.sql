9e9e6bb13a6400fb375dbc605788cb02a0431c04
Thang <thangdq2003038@ivy.edu.vn>
2024-07-26 21:29:36 +0700

    Buoi7

---
+++ Buoi7.sql
@@ -0,0 +1,31 @@
+create view product_quantity_view as
+select ProductID, sum(quantity)
+from orderdetails
+group by ProductID;
+
+select productName as name, unit,
+price * table_a.sum_of_quantity as total
+from products join (
+	select ProductID, sum(quantity) as 'sum_of_quantity'
+	from orderdetails
+	group by ProductID) as table_a
+on products.ProductID = table_A.ProductID;
+-- max: order cuoi cung (gan nhat)
+select max(OrderDate) as max_orderDate from orders;
+-- toan bo thong tin trong order
+select * from orders
+where OrderDate = (
+	select max(OrderDate) as max_orderDate from orders
+);
+
+-- avg: lay product co price > avg
+select * from products where price > (
+	select avg(price) as avg_price from products);
+-- gia trung binh tung loai sp > 30
+select category, avg(price) as avg_price
+from products group by category 
+having avg_price > 30;
+-- union: lien ket du lieu 2 bang
+select max(price) as range_price from products
+union
+select min(price) from products;
---
+++ Lab666.sql
@@ -0,0 +1,13 @@
+-- lay thong tin khach o Mexico
+
+-- Country trung nhau
+
+-- Customers: at Canada, Mexico, USA
+
+-- Ten cong ty: at USA => not SF
+
+-- ProductName -> name + unit (product): unit=> bottles
+
+-- danh sach san pham => sort theo gia
+
+-- 5 product (beverages) (price cao nhat)
