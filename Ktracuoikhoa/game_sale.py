ab19c1d5d37c7430e2a980fb2ade6e50f970c0b0
DiesFromCringe <dqthang25122009@gmail.com>
2024-09-08 20:54:17 +0700

    Ktracuoikhoa

--- Ktracuoikhoa/game_sale.py
+++ Ktracuoikhoa/game_sale.py
@@ -11,27 +11,67 @@ print(df.columns)
 fixed_df = df.loc[:, ['title','genre', 'critic_score', 'total_sales', 'na_sales', 'jp_sales','pal_sales', 'other_sales']] 
 print(fixed_df.describe())
 
-# # Top 10 bestseller games
-# n = 10 
-# titlelist = fixed_df['title'].value_counts().head(10).index.to_list()
-# index = np.arange(n)
-# width = 0.5
-# xvalue = fixed_df['title'].value_counts().head(10).sort_values(ascending=True)
-# bar1 = plt.barh(index, xvalue, width, color = 'green' )
-# plt.ylabel('Game Title')
-# plt.title('Top 10 bestseller games in 2024')
+# # top 10 games have highest total sales (wholeworld) (all versions)
+# group_by_title_df = fixed_df.groupby('title').sum()
+# group_by_title_df= group_by_title_df.sort_values(by='total_sales', ascending=False).head(10)
 
-# plt.yticks(index, titlelist[::-1])
+# # ve bieu do cot ngang 
+# plt.barh(group_by_title_df.index[::-1], group_by_title_df['total_sales'][::-1], 0.5, color='green')
+# plt.xlabel('Total Sales')
+# plt.ylabel('Title')
 # plt.show()
 
-# Top 10 genres 
-n = 10 
-titlelist = fixed_df['genre'].value_counts().head(10).index.to_list()
-index = np.arange(n)
-width = 0.5
-xvalue = fixed_df['genre'].value_counts().head(10).sort_values(ascending=True)
-bar1 = plt.barh(index, xvalue, width, color = 'green' )
-plt.ylabel('Game Genres')
-# Title để sao cô  
-plt.yticks(index, titlelist[::-1])
+
+# # top 10 games genre have highest total sales (wholeworld) (all versions)
+# group_by_title_df = fixed_df.groupby('genre').sum()
+# group_by_title_df= group_by_title_df.sort_values(by='total_sales', ascending=False).head(10)
+
+# # ve bieu do cot ngang 
+# plt.barh(group_by_title_df.index[::-1], group_by_title_df['total_sales'][::-1], 0.5, color='green')
+# plt.xlabel('Total Sales')
+# plt.ylabel('Title')
+# plt.show()
+
+
+# top 10 games genre have highest critic score (wholeworld) (all versions)
+# group_by_title_df = fixed_df.groupby('genre').sum()
+# group_by_title_df= group_by_title_df.sort_values(by='total_sales', ascending=False).head(10)
+
+# # ve bieu do cot ngang 
+# plt.barh(group_by_title_df.index[::-1], group_by_title_df['total_sales'][::-1], 0.5, color='green')
+# plt.xlabel('Total Sales')
+# plt.ylabel('Title')
+# plt.show()
+
+# # top 10 games have highest critic score (avg/ version)
+# highest_score_df = fixed_df.loc[:, ['title', 'critic_score']].groupby('title').mean()
+# highest_score_df= highest_score_df.sort_values(by='critic_score', ascending=False).head(10)
+# # ve bieu do cot ngang 
+# plt.barh(highest_score_df.index[::-1], highest_score_df['critic_score'][::-1], 0.5, color="green")
+# plt.xlabel('Critic score')
+# plt.ylabel('Title')
+# plt.show()
+
+# mối tương quan giữa critic score và total sales
+# plt.scatter(fixed_df['critic_score'], fixed_df['total_sales'], color="green")
+# plt.xlabel('Critic score')
+# plt.ylabel('Total sales')
+# plt.show()
+
+# # top 10 games have highest critic score (avg/ version)
+# highest_score_df = fixed_df.loc[:, ['title', 'critic_score']].groupby('title').mean()
+# highest_score_df= highest_score_df.sort_values(by='critic_score', ascending=False).head(10)
+# # ve bieu do cot ngang 
+# plt.barh(highest_score_df.index[::-1], highest_score_df['critic_score'][::-1], 0.5, color="green")
+# plt.xlabel('Critic score')
+# plt.ylabel('Title')
+# plt.show()
+
+# top 10 genres have highest total sales (wholeworld) (all versions)
+group_by_title_df = fixed_df.groupby('genre').sum()
+group_by_title_df= group_by_title_df.sort_values(by='total_sales', ascending=False)
+# ve bieu do cot ngang 
+plt.pie( group_by_title_df['total_sales'][::-1], labels=group_by_title_df.index[::-1], autopct='%1.1f%%')
+plt.show()
+# plt.legend(loc="best")
 plt.show()
